4bf2cf53a18a9c63bf40e7744bdae9163d39bd8e
martitasea <marta.pulido.asin@gmail.com>
2020-10-20 12:30:52 +0200

    Demo geolocateddemo that geolocates with browser coordinates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component}  from 'react';
+import { geolocated } from 'react-geolocated';
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const defaultLat= 40;
+const defaultLong=-3;
+
+
+class App extends Component {
+  render(){
+    const longitude=this.props.coords? 
+    this.props.coords.longitude
+    : defaultLong
+  
+    const latitude=this.props.coords? 
+    this.props.coords.latitude
+    : defaultLat
+  
+    return (
+     <Map center={[latitude, longitude]} zoom={12}>
+       <TileLayer
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}.png"
+              // attribution="Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community"
+            />
+        {
+          !this.props.coords?
+          <div>Loaging</div>
+          :<Marker position={[latitude, longitude]}>
+    
+            <Popup>You are here!</Popup>
+          </Marker>
+        }
+      </Map>
   );
-}
+}}
+export default geolocated({
+  positionOptions:{
+    enableHighAccuracy: false
+  },
+  userDecisionTimeout: 10000
+})(App);
 
-export default App;
+// export default App;
